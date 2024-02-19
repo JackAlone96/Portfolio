e1aeaa2b3710247fca86a7877797e5dfe4c1903f
JackAlone96 <giacalone.giovanni96@gmail.com>
2024-02-19 12:30:22 +0100

    Added _site folder

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "Memory Leak",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/A_Star/",
+        "teaser": "/assets/images/bio-photo.jpg"
+      },{
+        "title": "Memory Leak",
+        "excerpt":"Memory Leak - Scemo chi legge  ","categories": [],
+        "tags": [],
+        "url": "/portfolio/Memory_Leak/",
+        "teaser": "/assets/images/bio-photo.jpg"
+      },{
+        "title": "Memory Leak",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/Port_post_4/",
+        "teaser": "/assets/images/bio-photo.jpg"
+      },{
+        "title": "Memory Leak",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/Port_post_5/",
+        "teaser": "/assets/images/bio-photo.jpg"
+      },{
+        "title": "Memory Leak",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/Port_post_6/",
+        "teaser": "/assets/images/bio-photo.jpg"
+      },{
+        "title": "Memory Leak",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/portfolio/Port_post_7/",
+        "teaser": "/assets/images/bio-photo.jpg"
+      },{
+        "title": "Tenebris",
+        "excerpt":"Tenebris  ","categories": [],
+        "tags": [],
+        "url": "/Tenebris/",
+        "teaser": "/assets/images/bio-photo.jpg"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Ciao che fai?",
+        "excerpt":"Ciao che fai?  ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/ciao-che-fai/",
+        "teaser": null
+      }]
